f22cbcb37b94564037a3a29fcb879d3dd23308b0
abe <test>
2022-11-14 23:25:22 +0900

    Initial Commit

---
+++ tic_tac_toe.pyw
@@ -0,0 +1,165 @@
+"""
+ tk.Tk().withdraw()  ... ウィンドウ非表示
+ tk.Tk().deiconify() ... ウインドウ表示
+"""
+
+from tic_tac_toe_funcs import *
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import threading
+import time
+
+class TicTacGame:
+
+    def __init__(self):
+        self.size = 500
+        self.square_num = 3
+        self.root = tk.Tk()
+        self.root.withdraw()
+        self.root.geometry("{size}x{size}".format(size=self.size))
+        self.root.title("tic tac toe")
+
+        self.levels = ["level : low",  "level : medium",
+                       "level : high"]
+        self.level  = tk.StringVar()
+        self.cpu_strength = 0 # depth=[0,1,3]
+        self.list = ttk.Combobox(self.root, values=self.levels,
+                                 textvariable=self.level, height=1,
+                                 state="readonly", width=15)
+        self.list.current(0)
+        self.depth = self._get_depth()
+        self.list.bind("<<ComboboxSelected>>", lambda e:self._combo_selected())
+        self.list.pack()
+        
+        self.canvas = tk.Canvas(self.root, width=self.size, height=self.size)
+        self.canvas.bind("<Button-1>", self.click)
+        self.canvas.pack()
+
+        self.human = Human(Board.circle)
+        self.human.name = "あなた"
+        self.cpu   = Cpu(Board.cross)
+        self.board = Board(self.square_num, self.square_num)
+        self.turn  = 1
+
+        self.margin = self.size // 10
+        self.square_size = (self.size - self.margin*2) // self.square_num
+        self.repaint()
+        
+    def _get_depth(self):
+        lv_txt = self.level.get()
+        index = self.levels.index(lv_txt)
+        depths = [1, 3, 5]
+        return depths[index]
+
+    def _combo_selected(self):
+        self.depth = self._get_depth()
+        self.root.focus() # reset focus in listbox
+    
+    def play(self):
+        self.root.deiconify()
+        self.root.mainloop()
+
+    def click(self, event):
+        result = "ゲーム結果"
+        draw   = "引き分け"
+        win    = "{}の勝ち"
+        gameset = False
+        
+        def is_correct_index(x, y):
+            return x >= 0 and y >= 0 and x < self.square_num and y < self.square_num
+        
+        def put_player(x, y):
+            self.board.put(x, y, Board.circle)
+            self.repaint()
+            if self.board.is_won(self.human.mark):
+                messagebox.showinfo(result, win.format(self.human.name))
+                self.board.reset()
+                self.repaint()
+                nonlocal gameset; gameset = True
+                return
+            self.repaint()
+            
+            if not self.board.can_put():
+                messagebox.showinfo(result, draw)
+                self.board.reset()                
+                self.repaint()
+                gameset = True
+
+        def put_cpu(x, y):
+            if gameset:return
+            
+            if not self.board.can_put():
+                messagebox.showinfo(result, draw)
+                self.board.reset()
+                return
+
+            self.cpu.set_depth(self.depth)
+            self.cpu.put(self.board)
+
+            if self.board.is_won(self.cpu.mark):
+                self.repaint()
+                messagebox.showinfo(result, win.format(self.cpu.name))
+                self.board.reset()
+                self.repaint()
+                return
+            
+            if not self.board.can_put():
+                messagebox.showinfo(result, draw)
+                self.board.reset()
+                
+            self.repaint()
+            
+            
+        x = (event.x - self.margin) // self.square_size
+        y = (event.y - self.margin) // self.square_size
+        if not is_correct_index(x, y):
+            return
+        if [x, y] not in self.board.get_canputs():return
+
+        put_player(x, y)
+        put_cpu(x, y)
+        
+    def repaint(self):
+        cv = self.canvas
+        cv.delete('all')        
+        margin = self.margin
+        square_size = ((self.size - self.margin*2) / self.square_num)
+        p_size = 5
+        p_color= "white"
+        l_size = 3
+        l_color= ""
+        b_color= "#12FF90"
+
+        w1, h1 = margin, margin
+        w2, h2 = self.size - margin, self.size-margin
+        cv.create_rectangle(w1, h1, w2, h2, fill=b_color, width=0)
+
+        bar_num = int((self.size - (self.margin*2)) / square_size) +1
+        for i in range(bar_num):
+            x1 = margin
+            x2 = self.size - margin
+            y  = i * square_size + margin
+            cv.create_line(x1, y, x2, y, width=l_size)
+        for i in range(bar_num):
+            x1 = margin
+            x2 = self.size - margin
+            y  = i * square_size + margin
+            cv.create_line(y, x1, y, x2, width=l_size)
+        board = self.board.get()
+        for h, row in enumerate(board):
+            y1 = margin + h * square_size + square_size//4
+            y2 = y1 + square_size//2
+            for w, elm in enumerate(row):
+                x1 = margin + w * square_size + square_size//4
+                x2 = x1 + square_size//2
+                if elm == Board.circle:
+                    cv.create_oval(x1, y1, x2, y2, width=p_size, outline=p_color)
+                elif elm == Board.cross:
+                    cv.create_line(x1, y1, x2, y2, width=p_size, fill=p_color)
+                    cv.create_line(x1+square_size//2, y1, x2-square_size//2, y2,
+                                   width=p_size, fill=p_color)
+
+if __name__ == "__main__":
+    g = TicTacGame()
+    g.play()
---
+++ tic_tac_toe_funcs.py
@@ -0,0 +1,327 @@
+"""
+ 2020/08/11/Tue: マルバツゲームを作る
+                 random.sample(list, k) listをシャッフルしてk個の要素取得して返す
+                 itertools.cycle() で無限イテレータを作れる usage: next(iter)
+                 for i in itertools.count():print(i);if i > 10:break
+ 2020/08/12/Wed: mini-maxでcpu思考ルーチン作った。静的評価関数がミスってたり
+                 再帰が良くわからなかったりで2,3時間かかった。今もよくわかってない
+                 いや、理論はわかるが再帰での実装、手番の関係が微妙。
+                 minimaxで5手読めば負けないcpuは出来た。
+                 初手だけランダムで打てればもっと良い
+                 tkinterでマルバツゲームのguiも作った。
+                 guiの見栄えもよくしたい
+                 仕組みが完全には理解できておらず、何作ればいいかわからないからできない
+                 m,n,kゲーム k目並べ
+                 αβ法を実装する。
+                 minimax   -> 0.65s , 24424 node
+                 alphabeta -> 0.15s ,  3838 node
+                 だいぶ高速化できた
+"""
+import random
+import itertools
+import time
+import copy
+
+
+class TicTacToe:
+    
+    def __init__(self):
+        self.players = [Human, Cpu]
+        self.first_player  = None
+        self.second_player = None
+        self.decide_pturn()
+        self.board = Board(3, 3)
+        self.turn  = 1
+
+    def decide_pturn(self):
+        plis = random.sample(self.players, 2)
+        self.first_player  = plis[0](Board.circle)
+        self.second_player = plis[1](Board.cross)        
+    
+    def play(self):
+        self.turn = 1
+        players_ite = itertools.cycle([self.first_player, self.second_player])
+        while True:
+            p = next(players_ite)
+            print("TURN : {turn} {name} {mark}"
+                  .format(turn=self.turn, name=p.name, mark=Board.get_mark(p.mark)))
+            print(self.board)
+            if not self.board.can_put():                
+                break
+            p.put(self.board)
+            if self.board.is_won(p.mark):
+                print(self.board)
+                self.print_winner(p.name)
+                return
+            self.turn += 1
+            print()
+        self.print_draw()
+
+    def print_winner(self, name):
+        print(f"winner: {name}")
+
+    def print_draw(self):
+        print("draw")
+        
+
+class Board:
+
+    empty  =  0
+    circle =  1
+    cross  = -1
+    mark_map = {
+        empty  : "-",
+        circle : "◯",
+        cross  : "×"
+        }
+    
+    def __init__(self, width, height):
+        self.board = [[self.empty for i in range(width)]for j in range(height)]
+        self.width = width
+        self.height = height
+
+    def can_put(self):
+        cp = False
+        for row in self.board:
+            cp  = cp or not(all(row))
+        return cp
+
+    def is_won(self, mark):
+        exist_line = False
+        for y in range(self.height):
+            tmp = []
+            for x in range(self.width):
+                val = self.board[y][x]
+                tmp.append(val==mark)
+            exist_line = exist_line or all(tmp)
+        for x in range(self.height):
+            tmp = []
+            for y in range(self.width):
+                val = self.board[y][x]
+                tmp.append(val==mark)
+            exist_line = exist_line or all(tmp)
+        tmp = []
+        for i in range(self.height):
+            val = self.board[i][i]
+            tmp.append(val==mark)
+        exist_line = exist_line or all(tmp)
+        tmp = []
+        for i in range(self.height-1, -1, -1):
+            j = abs( i - (self.height-1) )
+            val = self.board[i][j]
+            tmp.append(val==mark)
+        exist_line = exist_line or all(tmp)
+            
+        return exist_line
+
+    def is_exist(self, x, y):
+        return self.board[y][x] != self.empty
+
+    def get_canputs(self):
+        coordinates = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.is_exist(x, y): continue
+                coordinates.append([x, y])
+        return coordinates                  
+    
+    def put(self, x, y, mark):
+        self.board[y][x] = mark
+
+    def __str__(self):
+        b = " " + "".join([str(i) for i in range(self.width)]) + "\n"
+        for cnt, row in enumerate(self.board):
+            b += str(cnt)
+            for s in row:
+                b += self.mark_map.get(s, s)
+            b += "\n"
+        return b.rstrip("\n")
+
+    def get(self):
+        return self.board
+
+    @classmethod
+    def get_mark(cls, mark):
+        return cls.mark_map.get(mark, mark)
+
+    def reset(self):
+        self.board =  [[self.empty for i in range(self.width)]for j in range(self.height)]
+
+    def count(self):
+        cnt =  0
+        for row in self.board:
+            for elm in row:
+                if elm != self.empty:
+                    cnt += 1
+        return cnt
+
+class Player:
+
+    def __init__(self, name, mark):
+        self.name = name
+        self.mark = mark
+
+    def put(self, board):
+        x, y = self.think(board)
+        board.put(x, y, self.mark)
+
+    def think(self, board):
+        raise NotImplementedError
+
+
+class Human(Player):
+
+    def __init__(self, mark):
+        super().__init__("Human", mark)
+
+    def think(self, board):
+        def get():
+            return map(int, input("enter your hand(x y): ").strip().split())
+        x, y = get()
+        while [x, y] not in board.get_canputs():
+            print(f"{x},{y} is an invalid coordinate.")
+            x, y = get()
+        return x, y
+
+
+class Cpu(Player):
+
+    def __init__(self, mark):
+        super().__init__("Cpu", mark)
+        self.depth = 5
+
+    def random(self, board):
+        return random.choice(board.get_canputs())
+
+    def static_value(self, board):
+        pt = 10
+        if board.is_won(-self.mark):
+            return -pt
+        if board.is_won(self.mark):
+            return pt
+        return 0
+    
+    def minmax_depth_1(self, board):
+        hand  = []
+        score = -99
+        canput = board.get_canputs()
+        random.shuffle(canput)
+        for xy in canput:
+            x, y = xy
+            b = copy.deepcopy(board)
+            b.put(x, y, self.mark)
+            s = self.static_value(b)
+            if s >= score:
+                score = s
+                hand = xy
+        return hand
+    
+    def minimax(self, board, depth):
+        node_num = 0
+        start = time.time()
+        def get_children(board, mark):
+            children = []
+            for h in board.get_canputs():
+                b = copy.deepcopy(board)
+                x, y = h
+                b.put(x, y, mark)
+                children.append(b)
+            nonlocal node_num
+            node_num += len(children)
+            return children
+                
+        def _minmax(self, board, mark, depth):
+            if depth == 0 or board.is_won(-mark):
+                return self.static_value(board)
+            children = get_children(board, mark)
+            if len(children) == 0:
+                return self.static_value(board)
+            if mark == self.mark:
+                max = -9999
+                for c in children:                
+                    score = _minmax(self, c, -1 * mark, depth-1)
+                    if score > max: max = score
+                return max
+            if mark == -self.mark:
+                min = 9999
+                for c in children:                
+                    score = _minmax(self, c, -1 * mark, depth-1)
+                    if score < min: min = score
+                return min
+        
+        children = get_children(board, self.mark)
+        hands = board.get_canputs()
+        max  = -9999
+        hand = []
+        for c,h in zip(children, hands):
+            score = (_minmax(self, c, -self.mark, depth=depth))
+            #print(c)
+            #print("last_score:"+str(score))
+            if score > max:
+                max = score
+                hand = h
+                end = time.time()
+                
+        print("elapsed time: ", end-start)
+        print("node number : ",node_num)
+        return hand
+
+    def alpha_beta(self, board, depth):
+        node_num = 0
+        start = time.time()
+        def get_children(board, mark):
+            children = []
+            for h in board.get_canputs():
+                b = copy.deepcopy(board)
+                x, y = h
+                b.put(x, y, mark)
+                children.append(b)
+            nonlocal node_num
+            node_num += len(children)
+            return children
+                
+        def _alpha_beta(self, board, mark, depth, a, b):
+            if depth == 0 or board.is_won(-mark):
+                return self.static_value(board)
+            children = get_children(board, mark)
+            if len(children) == 0:
+                return self.static_value(board)
+            if mark == self.mark:
+                for c in children:
+                    if a < b:
+                        score = _alpha_beta(self, c, -1 * mark, depth-1, a, b)
+                    if score > a: a = score
+                return a
+            if mark == -self.mark:
+                for c in children:
+                    if a < b:
+                        score = _alpha_beta(self, c, -1 * mark, depth-1, a, b)
+                    if score < b: b = score
+                return b
+        
+        children = get_children(board, self.mark)
+        hands = board.get_canputs()
+        map_ch = random.sample(list(zip(children, hands)), len(children))
+        max  = -9999
+        hand = []
+        for c,h in map_ch:
+            score = (_alpha_beta(self, c, -self.mark, depth, -9999, 9999))
+            if score > max:
+                max = score
+                hand = h
+                
+        end = time.time()
+        print("elapsed times  : ", end-start)
+        print("number of nodes: ",node_num)
+        return hand
+
+    def set_depth(self, lv):
+        self.depth = lv
+        
+    def think(self, board):
+        #return self.minimax(board, 5)
+        return self.alpha_beta(board, self.depth)
+
+if __name__ == "__main__":
+    g = TicTacToe()
+    g.play()
